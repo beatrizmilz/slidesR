2aebb5bca7ca1bd1baee37af2471e2a16170579d
Beatriz Milz <beatriz.milz@hotmail.com>
2020-08-15 15:22:40 -0300

    avançando na apresentação de introducao ao R

---
+++ pacotes_necessarios.R
@@ -0,0 +1,6 @@
+# Pacotes necessários
+
+devtools::install_github("gadenbuie/xaringanExtra", dependencies = TRUE)
+devtools::install_github("gadenbuie/xaringanthemer", dependencies = TRUE)
+install.packages("showtext")
+devtools::install_github("hadley/emo")
